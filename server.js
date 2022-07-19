59df5dc58fa7e285639760feccc71dc7939682d8
Joseph Jepson <joey@joeyjepson.com>
2022-07-19 09:23:04 -0700

    got server running and created sketch canvas

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+const server = app.listen(3005);
+
+app.use(express.static("public"));
+
 console.log("my socket server is running");
